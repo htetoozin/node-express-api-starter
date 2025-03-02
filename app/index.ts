41c0d63a1c36f130cd6e661f4c55da461042242b
htetoozin <htetoo.zin09@gmail.com>
2025-03-02 18:31:56 +0700

    fixed nodemaon watch

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from "express";
-import userRoute from "@routes/user";
+import userRoute from "./routes/user";
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello TypeScript with Node.js!" });
+  res.json({ message: "Hello Plar with Node.js!" });
 });
 
 app.use(userRoute);
