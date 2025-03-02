91f5a3de54a7df296b9ad9570eb5cdc68651cce2
htetoozin <htetoo.zin09@gmail.com>
2025-03-02 23:54:56 +0700

    add morgan logging in development

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,19 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+
 import apiRoute from "./routes/api";
 import welcomeRoute from "./routes/welcome";
+
+const morgan = require("morgan");
 
 dotenv.config();
 
 const app: Express = express();
+
+//Development Logging
+if (process.env.APP_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
