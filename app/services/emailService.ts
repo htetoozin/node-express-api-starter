4fddc284fc7e53f3a11d54267e4bb4b0db37ab54
htetoozin <htetoo.zin09@gmail.com>
2025-03-22 11:30:38 +0700

    modify email service

--- app/services/emailService.ts
+++ app/services/emailService.ts
@@ -53,7 +53,7 @@ export const sendEmail = async (
     const transporter = nodemailer.createTransport({
       host: mail.host,
       port: mail.port,
-      secure: false,
+      secure: mail.secure, // true for 465, false for other ports
       auth: {
         user: mail.username,
         pass: mail.password,
